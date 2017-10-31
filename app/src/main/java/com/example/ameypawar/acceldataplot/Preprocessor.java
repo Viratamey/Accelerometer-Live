afeba554bf847971a6510aefcf04d86bb291a8f6
Viratamey <amey.pawar137@gmail.com>
2017-11-01 00:46:50 +0530

    first commit

---
+++ app/src/main/java/com/example/ameypawar/acceldataplot/Preprocessor.java
@@ -0,0 +1,22 @@
+package com.example.ameypawar.acceldataplot;
+
+
+
+
+/**
+ * This interface represents types which are able to perform data processing in
+ * place. Useful examples include: eliminating zeros, padding etc.
+ *
+ * @author Marcin Rzeźnicki
+ * @see SGFilter#appendPreprocessor(Preprocessor)
+ */
+public interface Preprocessor {
+
+    /**
+     * Data processing method. Called on Preprocessor instance when its
+     * processing is needed
+     *
+     * @param data
+     */
+    void apply(double[] data);
+}
